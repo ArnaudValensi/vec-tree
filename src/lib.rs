75b42d5db9d0203005e74921c758646f55a99be9
Arnaud Valensi <arnaud.valensi@gmail.com>
2018-09-24 22:24:47 +0200

    Add generation-arena

--- src/lib.rs
+++ src/lib.rs
@@ -1,58 +1,123 @@
+extern crate generational_arena;
+use generational_arena::{Arena, Index};
+
 use core::ops;
-use std::{cmp, fmt, mem};
+use std::{fmt, mem};
 
 #[derive(Debug)]
 pub struct VecTree<T> {
-    nodes: Vec<Node<T>>,
+    nodes: Arena<Node<T>>,
 }
 
-#[derive(Debug)]
-pub struct Node<T> {
-    parent: Option<NodeId>,
-    previous_sibling: Option<NodeId>,
-    next_sibling: Option<NodeId>,
-    first_child: Option<NodeId>,
-    last_child: Option<NodeId>,
-
-    pub data: T,
+#[derive(Clone, Debug)]
+struct Node<T> {
+    parent: Option<Index>,
+    previous_sibling: Option<Index>,
+    next_sibling: Option<Index>,
+    first_child: Option<Index>,
+    last_child: Option<Index>,
+    data: T,
 }
 
-#[derive(Debug, Copy, Clone)]
-pub struct NodeId {
-    index: usize,
-}
+// #[derive(Debug, Copy, Clone)]
+// pub struct NodeId {
+//     index: Index,
+// }
+
+const DEFAULT_CAPACITY: usize = 4;
 
 impl<T> VecTree<T> {
     pub fn new() -> VecTree<T> {
-        VecTree { nodes: Vec::new() }
+        VecTree::with_capacity(DEFAULT_CAPACITY)
+    }
+
+    pub fn with_capacity(n: usize) -> VecTree<T> {
+        VecTree {
+            nodes: Arena::with_capacity(n),
+        }
     }
 
     #[inline]
-    pub fn insert(&mut self, data: T) -> NodeId {
-        let index = self.nodes.len();
+    pub fn reserve(&mut self, additional_capacity: usize) {
+        self.nodes.reserve(additional_capacity);
+    }
 
-        self.nodes.push(Node {
+    // TODO: Use value or data everywhere
+    #[inline]
+    pub fn try_insert(&mut self, data: T) -> Result<Index, T> {
+        let new_node = Node {
             parent: None,
             first_child: None,
             last_child: None,
             previous_sibling: None,
             next_sibling: None,
             data,
-        });
+        };
 
-        NodeId { index }
+        match self.nodes.try_insert(new_node) {
+            Ok(index) => Ok(index),
+            Err(Node { data, .. }) => Err(data),
+        }
+
+        // match self.free_list_head {
+        //     None => Err(data),
+        //     Some(i) => match self.nodes[i] {
+        //         Node::Occupied { .. } => panic!("corrupt free list"),
+        //         Node::Free { next_free } => {
+        //             self.free_list_head = next_free;
+        //             self.nodes[i] = Node::Occupied {
+        //                 parent: None,
+        //                 first_child: None,
+        //                 last_child: None,
+        //                 previous_sibling: None,
+        //                 next_sibling: None,
+        //                 generation: self.generation,
+        //                 data,
+        //             };
+        //             Ok(NodeId {
+        //                 index: i,
+        //                 generation: self.generation,
+        //             })
+        //         }
+        //     },
+        // }
     }
 
-    pub fn append_child(&mut self, node_id: NodeId, new_child_id: NodeId) {
+    #[inline]
+    pub fn insert(&mut self, data: T) -> Index {
+        let new_node = Node {
+            parent: None,
+            first_child: None,
+            last_child: None,
+            previous_sibling: None,
+            next_sibling: None,
+            data,
+        };
+
+        self.nodes.insert(new_node)
+    }
+
+    pub fn append_child(
+        &mut self,
+        node_id: Index,
+        new_child_id: Index,
+    ) -> Result<(), &'static str> {
         self.detach(new_child_id);
 
         let last_child_opt;
         {
-            let (node, new_child_node) = self.nodes.get_pair_mut(
-                node_id.index,
-                new_child_id.index,
-                "Can not append a node to itself",
-            );
+            let (node_opt, new_child_node_opt) = self.nodes.get2_mut(node_id, new_child_id);
+
+            if node_opt.is_none() {
+                return Err("The node you are trying to append to is invalid");
+            }
+
+            if new_child_node_opt.is_none() {
+                return Err("The node you are trying to append is invalid");
+            }
+
+            let node = node_opt.unwrap();
+            let new_child_node = new_child_node_opt.unwrap();
 
             new_child_node.parent = Some(node_id);
 
@@ -66,14 +131,17 @@ impl<T> VecTree<T> {
         }
 
         if let Some(last_child) = last_child_opt {
-            debug_assert!(self.nodes[last_child.index].next_sibling.is_none());
-            self.nodes[last_child.index].next_sibling = Some(new_child_id);
+            debug_assert!(self.nodes[last_child].next_sibling.is_none());
+            self.nodes[last_child].next_sibling = Some(new_child_id);
         }
+
+        Ok(())
     }
 
-    pub fn detach(&mut self, node_id: NodeId) {
+    // TODO: return error instead of panic
+    pub fn detach(&mut self, node_id: Index) {
         let (parent, previous_sibling, next_sibling) = {
-            let node = &mut self.nodes[node_id.index];
+            let node = &mut self.nodes[node_id];
             (
                 node.parent.take(),
                 node.previous_sibling.take(),
@@ -82,48 +150,49 @@ impl<T> VecTree<T> {
         };
 
         if let Some(next_sibling) = next_sibling {
-            self.nodes[next_sibling.index].previous_sibling = previous_sibling;
+            self.nodes[next_sibling].previous_sibling = previous_sibling;
         } else if let Some(parent) = parent {
-            self.nodes[parent.index].last_child = previous_sibling;
+            self.nodes[parent].last_child = previous_sibling;
         }
 
         if let Some(previous_sibling) = previous_sibling {
-            self.nodes[previous_sibling.index].next_sibling = next_sibling;
+            self.nodes[previous_sibling].next_sibling = next_sibling;
         } else if let Some(parent) = parent {
-            self.nodes[parent.index].first_child = next_sibling;
+            self.nodes[parent].first_child = next_sibling;
         }
     }
 
-    pub fn get(&self, node_id: NodeId) -> Option<&T> {
-        match self.nodes.get(node_id.index) {
+    pub fn get(&self, node_id: Index) -> Option<&T> {
+        match self.nodes.get(node_id) {
             Some(Node { ref data, .. }) => Some(data),
             _ => None,
         }
     }
 
-    pub fn get_mut(&mut self, node_id: NodeId) -> Option<&mut T> {
-        match self.nodes.get_mut(node_id.index) {
+    pub fn get_mut(&mut self, node_id: Index) -> Option<&mut T> {
+        match self.nodes.get_mut(node_id) {
             Some(Node { ref mut data, .. }) => Some(data),
             _ => None,
         }
     }
 
     pub fn capacity(&self) -> usize {
-        self.nodes.len()
+        println!("capacity");
+        self.nodes.capacity()
     }
 
     /// Return an iterator of references to this node’s children.
-    pub fn children(&self, node_id: NodeId) -> ChildrenIter<T> {
+    pub fn children(&self, node_id: Index) -> ChildrenIter<T> {
         ChildrenIter {
             tree: self,
-            node_id: self.nodes[node_id.index].first_child,
+            node_id: self.nodes[node_id].first_child,
         }
     }
 
     /// Return an iterator of references to this node and the siblings before it.
     ///
     /// Call `.next().unwrap()` once on the iterator to skip the node itself.
-    pub fn preceding_siblings(&self, node_id: NodeId) -> PrecedingSiblingsIter<T> {
+    pub fn preceding_siblings(&self, node_id: Index) -> PrecedingSiblingsIter<T> {
         PrecedingSiblingsIter {
             tree: self,
             node_id: Some(node_id),
@@ -133,7 +202,7 @@ impl<T> VecTree<T> {
     /// Return an iterator of references to this node and the siblings after it.
     ///
     /// Call `.next().unwrap()` once on the iterator to skip the node itself.
-    pub fn following_siblings(&self, node_id: NodeId) -> FollowingSiblingsIter<T> {
+    pub fn following_siblings(&self, node_id: Index) -> FollowingSiblingsIter<T> {
         FollowingSiblingsIter {
             tree: self,
             node_id: Some(node_id),
@@ -143,33 +212,11 @@ impl<T> VecTree<T> {
     /// Return an iterator of references to this node and its ancestors.
     ///
     /// Call `.next().unwrap()` once on the iterator to skip the node itself.
-    pub fn ancestors(&self, node_id: NodeId) -> AncestorsIter<T> {
+    pub fn ancestors(&self, node_id: Index) -> AncestorsIter<T> {
         AncestorsIter {
             tree: self,
             node_id: Some(node_id),
         }
-    }
-}
-
-impl<T> Node<T> {
-    pub fn parent(&self) -> Option<NodeId> {
-        self.parent
-    }
-
-    pub fn first_child(&self) -> Option<NodeId> {
-        self.first_child
-    }
-
-    pub fn last_child(&self) -> Option<NodeId> {
-        self.last_child
-    }
-
-    pub fn previous_sibling(&self) -> Option<NodeId> {
-        self.previous_sibling
-    }
-
-    pub fn next_sibling(&self) -> Option<NodeId> {
-        self.next_sibling
     }
 }
 
@@ -183,45 +230,16 @@ impl<T> fmt::Display for Node<T> {
     }
 }
 
-trait GetPairMut<T> {
-    /// Get mutable references to two distinct nodes. Panics if the two given IDs are the same.
-    fn get_pair_mut(
-        &mut self,
-        a: usize,
-        b: usize,
-        same_index_error_message: &'static str,
-    ) -> (&mut T, &mut T);
-}
-
-impl<T> GetPairMut<T> for Vec<T> {
-    fn get_pair_mut(
-        &mut self,
-        a: usize,
-        b: usize,
-        same_index_error_message: &'static str,
-    ) -> (&mut T, &mut T) {
-        if a == b {
-            panic!(same_index_error_message)
-        }
-        let (xs, ys) = self.split_at_mut(cmp::max(a, b));
-        if a < b {
-            (&mut xs[a], &mut ys[0])
-        } else {
-            (&mut ys[0], &mut xs[b])
-        }
-    }
-}
-
-impl<T> ops::Index<NodeId> for VecTree<T> {
+impl<T> ops::Index<Index> for VecTree<T> {
     type Output = T;
 
-    fn index(&self, index: NodeId) -> &Self::Output {
+    fn index(&self, index: Index) -> &Self::Output {
         self.get(index).unwrap()
     }
 }
 
-impl<T> ops::IndexMut<NodeId> for VecTree<T> {
-    fn index_mut(&mut self, index: NodeId) -> &mut Self::Output {
+impl<T> ops::IndexMut<Index> for VecTree<T> {
+    fn index_mut(&mut self, index: Index) -> &mut Self::Output {
         self.get_mut(index).unwrap()
     }
 }
@@ -229,12 +247,12 @@ impl<T> ops::IndexMut<NodeId> for VecTree<T> {
 macro_rules! impl_node_iterator {
     ($name:ident, $next:expr) => {
         impl<'a, T> Iterator for $name<'a, T> {
-            type Item = NodeId;
+            type Item = Index;
 
-            fn next(&mut self) -> Option<NodeId> {
+            fn next(&mut self) -> Option<Index> {
                 match self.node_id.take() {
                     Some(node_id) => {
-                        self.node_id = $next(&self.tree.nodes[node_id.index]);
+                        self.node_id = $next(&self.tree.nodes[node_id]);
                         Some(node_id)
                     }
                     None => None,
@@ -247,14 +265,14 @@ macro_rules! impl_node_iterator {
 /// An iterator of references to the children of a given node.
 pub struct ChildrenIter<'a, T: 'a> {
     tree: &'a VecTree<T>,
-    node_id: Option<NodeId>,
+    node_id: Option<Index>,
 }
 impl_node_iterator!(ChildrenIter, |node: &Node<T>| node.next_sibling);
 
 /// An iterator of references to the siblings before a given node.
 pub struct PrecedingSiblingsIter<'a, T: 'a> {
     tree: &'a VecTree<T>,
-    node_id: Option<NodeId>,
+    node_id: Option<Index>,
 }
 impl_node_iterator!(PrecedingSiblingsIter, |node: &Node<T>| node
     .previous_sibling);
@@ -262,13 +280,13 @@ impl_node_iterator!(PrecedingSiblingsIter, |node: &Node<T>| node
 /// An iterator of references to the siblings after a given node.
 pub struct FollowingSiblingsIter<'a, T: 'a> {
     tree: &'a VecTree<T>,
-    node_id: Option<NodeId>,
+    node_id: Option<Index>,
 }
 impl_node_iterator!(FollowingSiblingsIter, |node: &Node<T>| node.next_sibling);
 
 /// An iterator of references to the ancestors a given node.
 pub struct AncestorsIter<'a, T: 'a> {
     tree: &'a VecTree<T>,
-    node_id: Option<NodeId>,
+    node_id: Option<Index>,
 }
 impl_node_iterator!(AncestorsIter, |node: &Node<T>| node.parent);
--- tests/tests.rs
+++ tests/tests.rs
@@ -3,22 +3,18 @@ use vec_tree::VecTree;
 
 #[test]
 fn create_tree() {
-    let mut tree = VecTree::new();
+    let mut tree = VecTree::<i32>::new();
 
     let root_node_id = tree.insert(1);
     let child_node_id_1 = tree.insert(2);
     let child_node_id_2 = tree.insert(3);
 
-    tree.append_child(root_node_id, child_node_id_1);
-    tree.append_child(root_node_id, child_node_id_2);
+    tree.append_child(root_node_id, child_node_id_1).expect("valid");
+    tree.append_child(root_node_id, child_node_id_2).expect("valid");
 
-    assert!(tree.capacity() == 3, "it should have 3 nodes in the tree");
-    assert!(
-        *tree.get(root_node_id).unwrap() == 1,
-        "it should have 1 as data"
-    );
-
-    assert!(tree[child_node_id_1] == 2, "it should have 1 as data");
+    assert_eq!(tree.capacity(), 4);
+    assert_eq!(*tree.get(root_node_id).unwrap(), 1);
+    assert_eq!(tree[child_node_id_1], 2);
 }
 
 #[test]
@@ -39,10 +35,10 @@ fn iterate_over_children() {
     let child_node_3 = tree.insert(4);
     let grandchild = tree.insert(5);
 
-    tree.append_child(root_node, child_node_1);
-    tree.append_child(root_node, child_node_2);
-    tree.append_child(root_node, child_node_3);
-    tree.append_child(child_node_3, grandchild);
+    tree.append_child(root_node, child_node_1).expect("valid");
+    tree.append_child(root_node, child_node_2).expect("valid");
+    tree.append_child(root_node, child_node_3).expect("valid");
+    tree.append_child(child_node_3, grandchild).expect("valid");
 
     assert_eq!(
         tree
